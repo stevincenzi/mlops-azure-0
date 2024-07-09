eb44c90dff791a84725a51b76fbb8f26a6b693e1
s.vincenzi <s.vincenzi@ammagamma.com>
2024-07-09 15:01:33 +0200

    modified code checks pipeline

--- src/model/train.py
+++ src/model/train.py
@@ -1,20 +1,19 @@
 # Import libraries
 
 import argparse
-import glob
-import os
 import mlflow
-import mlflow.sklearn
+import mlflow.sklearn 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
-import numpy as np 
+import numpy as np
 from typing import Tuple
-from pathlib import Path 
+from pathlib import Path
 
 
-def split_data(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def split_data(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray,
+                                          np.ndarray, np.ndarray]:
     """
     Split the data into training and test sets.
 
@@ -27,12 +26,17 @@ def split_data(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np
         The training and test sets for the features (X) and target (y).
     """
     # split features from target
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    # mlflow logging 
+    X, y = df[
+        ['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+         'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']
+            ].values, df['Diabetic'].values
+    # mlflow logging
     mlflow.log_metric("num_samples", X.shape[0])
     # split data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,
+                                                        random_state=0)
     return X_train, X_test, y_train, y_test
+
 
 def main(args):
     # enable autologging
@@ -40,35 +44,37 @@ def main(args):
     mlflow.sklearn.autolog()
 
     # read data
-    df = get_csvs_df(args.training_data)
+    training_data_path = Path.cwd() / args.training_data
+    df = get_csvs_df(path=training_data_path)
 
     # split data
     X_train, X_test, y_train, y_test = split_data(df=df)
 
     # train model
     logistic = train_model(args.reg_rate, X_train, y_train)
-    
     # evaluate model
     evaluate(logistic, X_test, y_test)
-    
     # stop logging 
     mlflow.end_run()
 
 
 def get_csvs_df(path):
-    path = Path.cwd() / path
+    path = Path(path)
+    # path = Path.cwd() / path
     if not path.exists():
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = path.glob("*.csv")
+    csv_files = list(path.glob("*.csv"))
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
-    
+
 def train_model(reg_rate, X_train, y_train):
     # train model
-    logistic = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    logistic = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(
+        X_train, y_train)
     return logistic
+
 
 def evaluate(logistic, X_test, y_test):
     # evaluate model
@@ -77,10 +83,10 @@ def evaluate(logistic, X_test, y_test):
     mlflow.log_metric("accuracy", acc)
     # roc auc score 
     y_scores = logistic.predict_proba(X_test)
-    roc_auc = roc_auc_score(y_test, y_scores[:,1])
+    roc_auc = roc_auc_score(y_test, y_scores[:, 1])
     mlflow.log_metric("roc_auc", roc_auc)
-    return 
-    
+    return
+
 
 def parse_args():
     # setup arg parser
@@ -97,6 +103,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
